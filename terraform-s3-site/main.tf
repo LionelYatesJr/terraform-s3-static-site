67e193b7a0627608b8447382142ef194187a0cf7
LionelYatesJr <46111161+LionelYatesJr@users.noreply.github.com>
2025-07-03 12:43:42 -0400

    Add files via upload

---
+++ terraform-s3-site/main.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "random_id" "bucket_id" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "my-terraform-static-site-${random_id.bucket_id.hex}"
+
+  tags = {
+    Name        = "TerraformS3Website"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "static_site" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "website_block" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "allow_public_read" {
+  depends_on = [aws_s3_bucket_public_access_block.website_block]
+  bucket     = aws_s3_bucket.website_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = ["s3:GetObject"],
+        Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
+      }
+    ]
+  })
+}
